3b356113fd28d340c1714500253b6d3777cf05f1
Immanuel <57101108+immanuel404@users.noreply.github.com>
2020-07-22 19:14:24 +0200

    Add files via upload

---
+++ precache-manifest.4c5fec4cab27ad763e5a15fd9192a702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de99d89c294d8dd851c3e8d4adafeef2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aed650337df63bef4e8d",
+    "url": "/static/css/main.8228bbcc.chunk.css"
+  },
+  {
+    "revision": "84ad491e556dbae2283d",
+    "url": "/static/js/2.5251bb94.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.5251bb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed650337df63bef4e8d",
+    "url": "/static/js/main.5080a058.chunk.js"
+  },
+  {
+    "revision": "acdf868312f78a97cdd0",
+    "url": "/static/js/runtime-main.794c9aa6.js"
+  },
+  {
+    "revision": "947da76078151d0ce3726bd3baca1016",
+    "url": "/static/media/pic.947da760.jpg"
+  }
+]);
